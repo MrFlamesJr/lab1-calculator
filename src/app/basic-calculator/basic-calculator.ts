6bb7615199b756221174e9fd4116dd401626ecc1
MrFlamesJr <mathys.nitreb@gmail.com>
2026-09-17 15:34:07 -0400

    basic calculator layout

--- src/app/basic-calculator/basic-calculator.ts
+++ src/app/basic-calculator/basic-calculator.ts
@@ -6,4 +6,33 @@ import { Component } from '@angular/core';
   styleUrl: './basic-calculator.css',
   templateUrl: './basic-calculator.html',
 })
-export class BasicCalculator {}
+export class BasicCalculator {
+
+  answer: number | null = null;
+
+
+  calculate = (operator: string, first: string, second: string) => {
+    //strings because html inputs are strings
+
+    console.log(first, operator, second);
+    // calculate and set this.answer (this automatically sets it in the DOM because of [value]=)
+  }
+}
+
+
+
+
+
+
+/*
+  //possible solution:
+
+  operations: { [key: string]: (a: number, b: number) => number } = {
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '×': (a, b) => a * b,
+    '÷': (a, b) => a / b
+  };
+
+  this.operations[operator](Number(first), Number(second));
+ */
