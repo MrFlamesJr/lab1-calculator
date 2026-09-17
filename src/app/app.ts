638e9a7e352fee3c07f585573003f066f2fae411
MrFlamesJr <mathys.nitreb@gmail.com>
2026-09-16 20:33:15 -0400

    basic layout wip

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {BasicCalculator} from './basic-calculator/basic-calculator';  
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BasicCalculator],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
---
+++ src/app/basic-calculator/basic-calculator.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-basic-calculator',
+  styleUrl: './basic-calculator.css',
+  templateUrl: './basic-calculator.html',
+})
+export class BasicCalculator {}
