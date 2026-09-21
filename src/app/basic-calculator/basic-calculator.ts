cd169e514f3e37d0312abb27fdd90feb9893fc1c
Amelie Jehanne Lanto <alant044@uottawa.ca>
2026-09-20 20:48:37 -0400

    Ajout de la logique de la calculatrice

--- src/app/basic-calculator/basic-calculator.ts
+++ src/app/basic-calculator/basic-calculator.ts
@@ -14,7 +14,29 @@ export class BasicCalculator {
   calculate = (operator: string, first: string, second: string) => {
     // strings because html inputs are strings
 
-    console.log(first, operator, second);
-    // calculate and set this.answer (this automatically sets it in the DOM because of [value]=)
+    const a = parseFloat(first);
+    const b = parseFloat(second);
+
+    // si un des deux champs est vide ou invalide
+    if (isNaN(a) || isNaN(b)) {
+      this.answer = null;
+      return;
+    }
+
+    switch (operator) {
+      case '+':
+        this.answer = a + b;
+        break;
+      case '-':
+        this.answer = a - b;
+        break;
+      case '×':
+        this.answer = a * b;
+        break;
+      case '÷':
+        this.answer = b === 0 ? null : a / b; // évite la division par zéro
+        break;
+    }
+
   }
 }
